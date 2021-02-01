c55639924089140d06c9f2bb0f7ea5d651093968
lizhenwen727 <59024841+lizhenwen727@users.noreply.github.com>
2021-02-01 10:32:45 -0500

    finalized

--- app.js
+++ app.js
@@ -1,11 +1,106 @@
 let today = new Date()
-let currentYear = today.getFullYear(); 
-let currentDay = today.getDay() 
-let currentDate = today.getDate() 
-let currentMonth = today.getMonth() + 1 
+let currentYear = today.getFullYear();
+let currentDay = today.getDay()
+let currentDate = today.getDate()
+let currentMonth = today.getMonth() + 1
 let birthMonth = 7;
 let birthDay = 27;
 
+
+//add option to pick up a year
+let optionYear = document.querySelector('#select-year')
+for (let i = 2025; i >= 2000; i--) {
+  let option = document.createElement("option")
+  option.value = i;
+  option.innerText = i;
+  optionYear.append(option)
+}
+let months = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+//add options to pick up a month
+let optionMonth = document.querySelector('#select-month')
+for (let i = 0; i < months.length; i++) {
+  let option = document.createElement('option')
+  option.value = months[i]
+  option.innerText = months[i]
+  optionMonth.append(option)
+}
+
+//enable options
+let form = document.querySelector("#year-month-submission")
+form.addEventListener('change', (e) => {
+  e.preventDefault();
+  let selectYear = document.querySelector("#select-year").value
+  let selectMonth = document.querySelector("#select-month").value
+  let monthIndex = months.indexOf(selectMonth) + 1
+  createCalendar(selectYear, monthIndex)
+
+})
+
+
+//clear calendar each time;
+const clearCalendar = () => {
+  let calendarBody = document.querySelector('#calendar-body')
+  while (calendarBody.lastChild) {
+    calendarBody.removeChild(calendarBody.lastChild)
+  }
+}
+
+// display month,year above the calender
+function displayMonthYear(year, month) {
+  let showHeader = document.querySelector(".display-header")
+  let selectYear = document.querySelector("#select-year")
+  let selectMonth = document.querySelector("#select-month")
+  selectYear.value = year;
+  selectMonth.value = months[month - 1]
+  showHeader.innerText = `${months[month-1]} ${year}` //arr starts with index 0
+  getHoliday(year)
+  return showHeader
+}
+
+//create a calendar body 
+function createCalendar(year, month) {
+  //where the first of month start in the week
+  let firstDayofMonth = new Date(year, month - 1, 1).getDay() //weeks starts index=0 and sunday=0 
+
+  let totalDaysinMonth = new Date(year, month, 0).getDate()
+  changeBody(month)
+
+  displayMonthYear(year, month)
+
+  clearCalendar()
+
+  let day = 1; //everymonth start day=1
+  //loop to create row/data 
+  let calendarBody = document.querySelector('#calendar-body')
+  for (let i = 0; i < 7; i++) {
+    let tr = document.createElement("tr") //create row
+    tr.setAttribute("class", "calendar-row")
+    for (let z = 0; z < 7; z++) { //seven days a week
+      //on the first row, indicate first day of month start in week
+      //empty html element
+      if (i === 0 && z < firstDayofMonth) {
+        let td = document.createElement("td")
+        td.innerText = ""
+        tr.append(td) ////each row should have data with empty date
+      } else {
+        if (day > totalDaysinMonth) { //loop stop when reach to total days
+          break;
+        }
+        let td = document.createElement("td")
+        //highlight today;
+        if (day === currentDate && month === currentMonth && year === currentYear) {
+          td.setAttribute("class", "itstoday")
+        }
+        td.innerText += day;
+        tr.append(td)
+        day++; //increment day
+      }
+    }
+    calendarBody.append(tr) //insert row into calendar body
+  }
+  return calendarBody;
+}
 
 //fetch holiday data
 const getHoliday = async (year) => {
@@ -75,7 +170,6 @@ function getValue(holidays) {
   })
 }
 
-
 //fetch weather data
 const getWeather = async (zipcode) => {
   try {
@@ -116,100 +210,6 @@ const getZip = () => {
 const seachButton = document.querySelector('#search')
 seachButton.addEventListener('click', getZip)
 
-//add option to pick up a year
-let optionYear = document.querySelector('#select-year')
-for (let i = 2025; i >= 2000; i--) {
-  let option = document.createElement("option")
-  option.value = i;
-  option.innerText = i;
-  optionYear.append(option)
-}
-let months = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-//add options to pick up a month
-let optionMonth = document.querySelector('#select-month')
-for (let i = 0; i < months.length; i++) {
-  let option = document.createElement('option')
-  option.value = months[i]
-  option.innerText = months[i]
-  optionMonth.append(option)
-}
-
-
-//enable options
-let form = document.querySelector("#year-month-submission")
-form.addEventListener('change', (e) => {
-  e.preventDefault();
-  let selectYear = document.querySelector("#select-year").value
-  let selectMonth = document.querySelector("#select-month").value
-  let monthIndex = months.indexOf(selectMonth) + 1
-  createCalendar(selectYear, monthIndex)
-
-})
-
-
-//clear calendar each time;
-const clearCalendar = () => {
-  let calendarBody = document.querySelector('#calendar-body')
-  while (calendarBody.lastChild) {
-    calendarBody.removeChild(calendarBody.lastChild)
-  }
-}
-
-// display month,year above the calender
-function displayMonthYear(year, month) {
-  let showHeader = document.querySelector(".display-header")
-  let selectYear = document.querySelector("#select-year")
-  let selectMonth = document.querySelector("#select-month")
-  selectYear.value = year;
-  selectMonth.value = months[month - 1]
-  showHeader.innerText = `${months[month-1]} ${year}` //arr starts with index 0
-  getHoliday(year)
-  return showHeader
-}
-
-//create a calendar body 
-function createCalendar(year, month) {
-  //where the first of month start in the week
-  let firstDayofMonth = new Date(year, month - 1, 1).getDay() //weeks starts index=0 and sunday=0 
- 
-  let totalDaysinMonth = new Date(year, month, 0).getDate()
-  changeBody(month)
-
-  displayMonthYear(year, month)
-
-  clearCalendar()
-
-  let day = 1; //everymonth start day=1
-  //loop to create row/data 
-  let calendarBody = document.querySelector('#calendar-body')
-  for (let i = 0; i < 7; i++) {
-    let tr = document.createElement("tr") //create row
-    tr.setAttribute("class", "calendar-row")
-    for (let z = 0; z < 7; z++) { //seven days a week
-      //on the first row, indicate first day of month start in week
-      //empty html element
-      if (i === 0 && z < firstDayofMonth) {
-        let td = document.createElement("td")
-        td.innerText = ""
-        tr.append(td) ////each row should have data with empty date
-      } else {
-        if (day > totalDaysinMonth) { //loop stop when reach to total days
-          break;
-        }
-        let td = document.createElement("td")
-        //highlight today;
-        if (day === currentDate && month === currentMonth && year === currentYear) {
-          td.setAttribute("class", "itstoday")
-        }
-        td.innerText += day;
-        tr.append(td)
-        day++; //increment day
-      }
-    }
-    calendarBody.append(tr) //insert row into calendar body
-  }
-  return calendarBody;
-}
 
 //change  page body colors based on its season.
 function changeBody(month) {
